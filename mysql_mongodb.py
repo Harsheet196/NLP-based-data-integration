e32c65aa9206db37e3d234d3cb4319bdfd7d4ae2
Harsheet Chordiya <harsheetchordiya@gmail.com>
2023-04-09 21:34:37 +0530

    migration successful

--- mysql_mongodb.py
+++ mysql_mongodb.py
@@ -1,41 +1,39 @@
 import mysql.connector
 import pymongo
 import datetime
-import sql_connector
-import mongodb_connector
-mgoclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
-mgodb = mgoclient["temp4"]
-db = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    passwd="",
-    database="banking_db"
-)
 
-mycursor = db.cursor()
-mycursor.execute("show tables")
-tables = mycursor.fetchall()
+def migrate(host_mo, host_mysql, client_mo, db_mysql, user_mysql, pass_mysql):
+    mgoclient = pymongo.MongoClient(host_mo)
 
-for table in tables:
-    print("Table:"+table[0])
-    cursor = db.cursor(dictionary=True)
-    sql = "select * from " + table[0]
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    # print(result)
-    mgocol = mgodb[table[0]]
-    for row in result:
-        print("row data is:"+str(row))
-        for key in row:
-            if (isinstance(row[key], datetime.datetime) or isinstance(row[key], datetime.date)):
-                row[key] = row[key].strftime('%Y-%m-%d %H:%M:%S')
-            # print(key)
-        try:
-            mgocol.insert_one(row)
-        except:
-            print("error insert")
+    mgodb = mgoclient[client_mo]
+    db = mysql.connector.connect(
+        host=host_mysql,
+        user=user_mysql,
+        passwd=pass_mysql,
+        database=db_mysql
+    )
 
-    # migrate(db, mgodb, table[0], 1)
+    mycursor = db.cursor()
+    mycursor.execute("show tables")
+    tables = mycursor.fetchall()
 
-print("Done")
+    for table in tables:
+        print("Table:"+table[0])
+        cursor = db.cursor(dictionary=True)
+        sql = "select * from " + table[0]
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        # print(result)
+        mgocol = mgodb[table[0]]
+        for row in result:
+            #print("row data is:"+str(row))
+            for key in row:
+                if (isinstance(row[key], datetime.datetime) or isinstance(row[key], datetime.date)):
+                    row[key] = row[key].strftime('%Y-%m-%d %H:%M:%S')
+                # print(key)
+            try:
+                mgocol.insert_one(row)
+            except:
+                print("error insert")
+    return (True)
