61f92ee725eb5f1732f5ec7739b471f611ed4075
Harsheet Chordiya <harsheetchordiya@gmail.com>
2023-04-10 00:32:13 +0530

    Flask app updated

---
+++ webserver/index.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, redirect, url_for
+import mysql_mongodb as connector
+
+app = Flask(__name__)
+
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome your login success: %s' % name
+
+
+@app.route('/thanks', methods=['post'])
+def hello_world():
+    host_mo = request.form['host_mo']
+    host_mysql = request.form['host_mysql']
+    client_mo = request.form['client_mo']
+    db_mysql = request.form['db_mysql']
+    user_mysql = request.form['user_mysql']
+    pass_mysql = request.form['pass_mysql']
+    val = connector.migrate(host_mo, host_mysql, client_mo,
+                            db_mysql, user_mysql, pass_mysql)
+    print(val)
+    return redirect(url_for('success', name=val))
+
+
+if __name__ == '__main__':
+    app.run()
